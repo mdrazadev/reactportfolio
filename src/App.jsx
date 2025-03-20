0d4ad6ed1d38d2dcea2d9fdb72530c58d75399f2
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2025-03-21 01:39:33 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.css';
+import './index.css';
+import { LoadingScreen } from './components/LoadingScreen';
+import { useState } from 'react';
+import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* SHOWING LOADER */}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}     {/* onComplete is a prob */}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
