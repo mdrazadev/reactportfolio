c5960c4c19f791995c4885c9aaca6b4de097729c
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2025-04-11 20:07:14 +0530

    responsive:home & contact

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -17,9 +17,9 @@ export const Contact = () => {
             setFormData({ name: "", email: "", message: "" });
         }).catch(() => alert("Something went wrong. Please try again."));
     }
-    return <section id="contact" className="min-h-screen flex items-center justify-center py-20">
+    return <section id="contact" className="min-h-screen w-full flex items-center justify-center py-20">
         <RevealOnScroll>
-            <div className="px-4 w-150">
+            <div className="px-4 w-full sm:w-150">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center" >
                     Get In Touch
                 </h2>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -2,24 +2,24 @@ import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Home = () => {
     return (
-        <section id="home" className="min-h-screen flex justify-center items-center relative">
+        <section id="home" className="min-h-screen w-full flex justify-center items-center relative">
             < RevealOnScroll>
                 <div className="text-center z-10 px-4">
-                    <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
+                    <h1 className="text-3xl sm:text-5xl md:text-7xl font-bold sm:mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
                         Hi, I am Mohd Raza
                     </h1>
-                    <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                    <p className="text-gray-400 text-sm sm:text-lg mb-8 max-w-lg mx-auto">
                         A Flutter Developer
                     </p>
-                    <div className="flex justify-center space-x-4">
+                    <div className="flex justify-center space-x-2 sm:space-x-4">
                         <a
                             href="#projects"
-                            className="bg-blue-500 text-white px-6 py-3 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
+                            className="bg-blue-500 text-white px-2 sm:px-6 py-3 sm:py-3 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
                             View Projects
                         </a>
                         <a
                             href="#contact"
-                            className="border border-blue-500/50 text-blue-500 px-6 py-3 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10">
+                            className="border border-blue-500/50 text-blue-500 px-2 sm:px-6 py-3 sm:py-3 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10">
                             Contact Me
                         </a>
                     </div>
